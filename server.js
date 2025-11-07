c41b2c088db174e1980e2c3195088a0afa336948
lollear <azevallospizarro@gmail.com>
2025-11-06 20:40:13 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "EdubotKing Backend Running 🚀"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en el puerto:", PORT);
+});
